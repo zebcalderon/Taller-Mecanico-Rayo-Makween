23a5504f4f1330220e30def24d87c5a78bcc171f
zebcalderon <seb.calderon@duocuc.cl>
2024-04-25 17:32:05 -0400

    scripts

--- scripts_seba.js
+++ scripts_seba.js
@@ -12,27 +12,36 @@ function sumar(){
 // FUNCIÓN PARA TOMAR INFORMACIÓN DE INPUTS
 function carta() {
 
-    var vrut=String(document.getElementById("rut").value);
-    var vnombre=String(document.getElementById("nombre").value);
-    var vappaterno=String(document.getElementById("appaterno").value);
-    var vapmaterno=String(document.getElementById("apmaterno").value);
-    var vnacimiento=Date(document.getElementById("nacimiento").value);
-    var vedad=parseInt(document.getElementById("edad").value);
-    var vgenero=String(document.getElementById("genero").value);
-    var vcorreo=String(document.getElementById("correo").value);
-    var vcelular=parseInt(document.getElementById("celular").value);
-    var vprofesion=String(document.getElementById("profesion").value);
-    var vmotivacion=String(document.getElementById("motivo").value);
-
+    var vrut=document.getElementById("rut").value;    
+    // var vnombre=document.getElementById("nombre").value;
+    // var vappaterno=document.getElementById("appaterno").value;
+    // var vapmaterno=document.getElementById("apmaterno").value;
+    // var vnacimiento=Date(document.getElementById("nacimiento").value);
+    // var vedad=parseInt(document.getElementById("edad").value);
+    // var vgenero=document.getElementById("genero").value;
+    // var vcorreo=document.getElementById("correo").value;
+    // var vcelular=parseInt(document.getElementById("celular").value);
+    // var vprofesion=document.getElementById("profesion").value;
+    // var vmotivacion=document.getElementById("motivo").value;
     document.getElementById("rrut").value=vrut;
-    document.getElementById("rnombre").value=vnombre;
-    document.getElementById("rappaterno").value=vappaterno;
-    document.getElementById("rapmaterno").value=vapmaterno;
-    document.getElementById("rnacimiento").value=vnacimiento;
-    document.getElementById("redad").value=vedad;
-    document.getElementById("rgenero").value=vgenero;
-    document.getElementById("rcorreo").value=vcorreo;
-    document.getElementById("rcelular").value=vcelular;
-    document.getElementById("rprofesion").value=vprofesion;
-    document.getElementById("rmotivo").value=vnombre;
-}   
+    // document.getElementById("rnombre").value=vnombre;
+    // document.getElementById("rappaterno").value=vappaterno;
+    // document.getElementById("rapmaterno").value=vapmaterno;
+    // document.getElementById("rnacimiento").value=vnacimiento;
+    // document.getElementById("redad").value=vedad;
+    // document.getElementById("rgenero").value=vgenero;
+    // document.getElementById("rcorreo").value=vcorreo;
+    // document.getElementById("rcelular").value=vcelular;
+    // document.getElementById("rprofesion").value=vprofesion;
+    // document.getElementById("rmotivo").value=vnombre;
+
+    console.log("Input values:", vrut);
+}
+
+
+$(function(){
+    $("#enviarcarta").click(function(){
+        $("#rrut").val("233232");
+    })
+    
+});
