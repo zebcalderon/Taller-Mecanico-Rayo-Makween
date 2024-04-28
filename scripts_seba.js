9cbfca68936c1963f79b9e9aa4ac608567b8823a
zebcalderon <seb.calderon@duocuc.cl>
2024-04-27 22:12:51 -0400

    validaciones

--- scripts_seba.js
+++ scripts_seba.js
@@ -12,36 +12,30 @@ function sumar(){
 // FUNCIÓN PARA TOMAR INFORMACIÓN DE INPUTS
 function carta() {
 
-    var vrut=document.getElementById("rut").value;    
-    // var vnombre=document.getElementById("nombre").value;
-    // var vappaterno=document.getElementById("appaterno").value;
-    // var vapmaterno=document.getElementById("apmaterno").value;
-    // var vnacimiento=Date(document.getElementById("nacimiento").value);
-    // var vedad=parseInt(document.getElementById("edad").value);
-    // var vgenero=document.getElementById("genero").value;
-    // var vcorreo=document.getElementById("correo").value;
-    // var vcelular=parseInt(document.getElementById("celular").value);
-    // var vprofesion=document.getElementById("profesion").value;
-    // var vmotivacion=document.getElementById("motivo").value;
-    document.getElementById("rrut").value=vrut;
-    // document.getElementById("rnombre").value=vnombre;
-    // document.getElementById("rappaterno").value=vappaterno;
-    // document.getElementById("rapmaterno").value=vapmaterno;
-    // document.getElementById("rnacimiento").value=vnacimiento;
-    // document.getElementById("redad").value=vedad;
-    // document.getElementById("rgenero").value=vgenero;
-    // document.getElementById("rcorreo").value=vcorreo;
-    // document.getElementById("rcelular").value=vcelular;
-    // document.getElementById("rprofesion").value=vprofesion;
-    // document.getElementById("rmotivo").value=vnombre;
+    var vrut=document.getElementById("rut").value;
+    var vpnombre=document.getElementById("nombre").value;  
+    var vappaterno=document.getElementById("appaterno").value;
+    var vapmaterno=document.getElementById("apmaterno").value;
+    var vnacimiento=document.getElementById("nacimiento").value;
+    var vedad=document.getElementById("edad").value;
+    var vcorreo=document.getElementById("correo").value;
+    var vcelular=document.getElementById("celular").value;
+    var vprofesion=document.getElementById("profesion").value;
+    var vmotivacion=document.getElementById("motivo").value;
+    var vgenero=document.getElementById("genero").value;
 
-    console.log("Input values:", vrut);
+    var vnombre = vpnombre + " " + vappaterno + " " + vapmaterno;
+
+
+    rrut.textContent = vrut;
+    rnombre.textContent = vnombre;
+    rnacimiento.textContent = vnacimiento;
+    redad.textContent = vedad;
+    rcorreo.textContent = vcorreo;
+    rcelular.textContent = vcelular;
+    rprofesion.textContent = vprofesion;
+    // rmotivo.textContent = vmotivo;
+    rgenero.textContent = vgenero;
+
+    console.log("Input values:", vcelular);
 }
-
-
-$(function(){
-    $("#enviarcarta").click(function(){
-        $("#rrut").val("233232");
-    })
-    
-});
