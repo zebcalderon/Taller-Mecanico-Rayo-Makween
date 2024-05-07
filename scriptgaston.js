de8f0c89f68d9c7bb17163ddd6f13c11b01b2482
Gaston Zamora <gas.zamora@duocuc.cl>
2024-05-07 18:44:56 -0400

    valida sing in

--- scriptgaston.js
+++ scriptgaston.js
@@ -1,18 +1,29 @@
-$(document).ready(function(){
+const email = document.getElementById("email")
+const pass = document.getElementById("pass")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-    $("#1").mouseenter(function(){
-        $("#1").animate({  
-            opacity: '1',
-            height: '50px'
-          });
-    });
 
-    $("#1").mouseleave(function(){
-        $("#1").animate({     
-            opacity: '0.76',
-            height: '40px',
-            position: 'fixed'
-          });
-    });
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,4})+$/
+    parrafo.innerHTML = ""
 
-});
+    if(!regexEmail.test(email.value)){
+        warnings += 'El email no es valido<br>'
+        entrar = true
+    }
+
+    if(pass.value.length < 5) {
+        warnings += 'La contraseña no es valida'
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    } else {
+        parrafo.innerHTML = "Enviado"
+    }
+})
