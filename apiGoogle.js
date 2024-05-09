e0f21283ab538c4375383d9b30789d3bdf929262
Gaston Zamora <gas.zamora@duocuc.cl>
2024-05-09 15:37:46 -0400

    a

--- apiGoogle.js
+++ apiGoogle.js
@@ -1,7 +1,7 @@
 function iniciarMap(){
     let coord = {lat:-32.9701322 ,lng: -71.5121999};
     let map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
+      zoom: 12,
       center: coord
     });
     let marker = new google.maps.Marker({
