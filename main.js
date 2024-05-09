a6c448f22ce35f26c0664e5e8822c125c610527e
zebcalderon <seb.calderon@duocuc.cl>
2024-05-09 15:35:36 -0400

    borrando un sumar que no se usa

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-function sumar(){
-
-    var v1=parseInt(document.getElementById("valor1").value);
-    var v2=parseInt(document.getElementById("valor2").value);
-    var suma=0;
-    
-    suma=v1+v2;
-
-    document.getElementById("resultado").value=suma;
-}
-
 // FUNCIÓN PARA TOMAR INFORMACIÓN DE INPUTS
 function carta() {
 
