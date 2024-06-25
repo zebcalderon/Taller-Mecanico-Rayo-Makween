a278ee30966a0185f3e32adb94908f2f24726651
zebcalderon <seb.calderon@duocuc.cl>
2024-06-25 16:32:36 -0400

    Cambios de DDjango

--- taller/settings.py
+++ taller/settings.py
@@ -134,7 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
+#      os.path.join(BASE_DIR, 'static'),
 # ]
