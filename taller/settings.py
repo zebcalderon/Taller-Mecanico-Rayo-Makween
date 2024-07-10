773cf883d2ce26499390b13977b223b70ba151e2
zebcalderon <seb.calderon@duocuc.cl>
2024-07-10 15:57:45 -0400

    borrando una cosa

--- taller/settings.py
+++ taller/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
     }
 }
 
+LANGUAGE_CODE = 'es'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -115,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
